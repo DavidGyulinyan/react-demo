ee700430e28e747a852e63e570b6bc87f8c1935e
Davit Gyulinyan <93977518+DavidGyulinyan@users.noreply.github.com>
2022-08-04 15:41:44 +0400

    test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+function Name(props) {
+  return (
+    <span>{props.name}</span>
+  )
+};
+
+function Surname(props) {
+  return (
+    <span>{props.surname}</span>
+  )
+};
+
+function Age(props) {
+  return (
+    <span>{props.age}</span>
+  )
+};
+
+function Country(props) {
+  return (
+      <span>{props.country}</span>
+  )
+};
+
+function User(props) {
+  return (
+    <div>
+      <h3>
+        I'm <Name name={props.name} />  <Surname surname={props.surname} /> and I'm <Age age={props.age} /> from <Country country={props.country} />
+      </h3>
+    </div>
+  )
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <User
+          name={"Elon"}
+          surname={"Musk"}
+          age={51}
+          country={"USA"}
+        />
+
+        <User
+          name={"David"}
+          surname={"Moor"}
+          age={28}
+          country={"USA"}
+        />
+
+        <User
+          name={"Sarah"}
+          surname={"Doe"}
+          age={21}
+          country={"UK"}
+        />
+
       </header>
     </div>
-  );
-}
+  )
+};
 
 export default App;
